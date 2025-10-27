f04b63eac44ca0872255169f850aefb3a821b8e7
fluoroom <nico.vial.giudici@gmail.com>
2025-10-27 09:49:42 -0300

    ask what to do when exiting

--- alpine.sh
+++ alpine.sh
@@ -431,8 +431,21 @@ else
 fi
 
 if [ $ALREADYMOUNTED = "yes" ] ; then
-	echo "Umount is being skipped, as the rootfs was mounted already. You're now at your kindle's shell again."
+	echo "Umount is being skipped, as the rootfs was mounted already. Do you want to force stop? (y/n) Default: N."
+	read -r FORCE_STOP
+	if [ "$FORCE_STOP" = "y" ] || [ "$FORCE_STOP" = "Y" ]; then
+		umount_alpine
+	fi
 else
+	echo "Alpine will be unmounted as there was no previous mount. Do you want to keep it running? (y/n) Default: N."
+	read -r KEEP_RUNNING
+	if [ "$KEEP_RUNNING" = "y" ] || [ "$KEEP_RUNNING" = "Y" ]; then
+		echo "Keeping Alpine running"
+	else
+		umount_alpine
+	fi
+}
+umount_alpine() {
 	echo "You returned from Alpine, killing remaining processes"
 	# Kill processes if they exist, but don't fail if they don't
 	if pgrep Xephyr > /dev/null 2>&1; then
@@ -533,4 +546,4 @@ else
 	rmdir /tmp/alpine 2>/dev/null || true
 
 	echo "All done, you're now back at your kindle's shell."
-fi
+}
